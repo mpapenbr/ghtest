c14b47833a03ba72e9adbbdab3e09a9789b2abf1
mpapenbr <Markus.Papenbrock@gmail.com>
2020-08-05 20:22:20 +0200

    Workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
@@ -10,12 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>Mode is {process.env.NODE_ENV}</p>
+        <p>Value is {process.env.REACT_APP_MY_VAR}</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
